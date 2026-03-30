18e85e42bef038ee2919e4665a408bd8d006693b
Heitor Sales <desenvolvheitor@gmail.com>
2026-03-30 12:46:44 -0300

    Otimização do dataHoraHeader

--- src/main.js
+++ src/main.js
@@ -1,13 +1,7 @@
 // Atualização automática de data e hora
 const dataHoraHeader = document.getElementById("data-hora-header")
-const options = {
-    weekday: "long",
-    year: "numeric",
-    month: "long",
-    day: "2-digit"
-}
 
 setInterval(() => {
     let hoje = new Date()
-    dataHoraHeader.textContent = `${hoje.toLocaleDateString("pt-br", options)} • ${hoje.toLocaleTimeString()}` 
+    dataHoraHeader.textContent = `${hoje.toLocaleString("pt-br", { dateStyle: "long", timeStyle: "medium" }).replace("às", "•")}` 
 }, 1000)
