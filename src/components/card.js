4e8bcf13618a462b0d72354a70386aa49b111a18
Heitor Sales <desenvolvheitor@gmail.com>
2026-03-31 15:54:17 -0300

    Melhorias no card

--- src/components/card.js
+++ src/components/card.js
@@ -1,42 +1,73 @@
+import { consumirApi } from "../api/rickAndMorty.js"
+import { abrirModalPersonagem } from "./modal.js"
+
 const galeriaPersonagens = document.getElementById("galeria-personagens")
 const campoBusca = document.getElementById("campo-busca")
 const campoStatus = document.getElementById("status")
+const campoEspecie = document.getElementById("especies")
+const campoGenero = document.getElementById("generos")
 
-export async function criarCardPersonagem() {
-    try {
-        galeriaPersonagens.innerHTML = ""
+export async function criarCardPersonagem(linkRequisicao = `https://rickandmortyapi.com/api/character/?`) {
+        galeriaPersonagens.innerHTML = "Carregando..."
         
-        let linkRequisicao = `https://rickandmortyapi.com/api/character/?`
         if (campoBusca.value.length > 0) {
             linkRequisicao += `&name=${campoBusca.value}`
+        }
+
+        if (campoEspecie.value !== "") {
+            linkRequisicao += `&species=${campoEspecie.value}`
         }
 
         if (campoStatus.value !== "") {
             linkRequisicao += `&status=${campoStatus.value}`
         }
 
-        let resposta = await fetch(linkRequisicao)
-
-        if (!resposta.ok) {
-            throw new Error(`Erro na rede: ${resposta.status}`)
+        if (campoGenero.value !== "") {
+            linkRequisicao += `&gender=${campoGenero.value}`
         }
 
-        const dados = await resposta.json()
-        dados.results.forEach(dado => {
+        const dados = await consumirApi(linkRequisicao)
+        galeriaPersonagens.innerHTML = ""
+        dados.results.forEach((dado, indice) => {
+
+
             let card = document.createElement("li")
             card.setAttribute("class", "card-personagem")
+            card.onclick = () => abrirModalPersonagem(dado.id)
+            card.style = `animation-delay: ${indice * 0.015}s`
+
+            let statusColor
+            let statusEmoji
+            switch (dado.status.toString().toLowerCase()) {
+                case "alive":
+                    statusColor = "var(--alive-color)"
+                    statusEmoji = "&#x1FAC0;"
+                    break
+                case "dead":
+                    statusColor = "var(--dead-color)"
+                    statusEmoji = "&#x1F480;"
+                    break
+                case "unknown":
+                    statusColor = "var(--unknown-color)"
+                    statusEmoji = "&#x2753;"
+                    break
+            }
+
+            card.style.backgroundColor = statusColor
+            
             card.innerHTML = `<img src="${dado.image}" alt="" class="foto-personagem">
-                <div class="informacoes-personagem">
-                    <h2>${dado.name}</h2>
-                    <p>${dado.status} - ${dado.species}</p>
-                </div>`
+            <div class="informacoes-card-personagem">
+                <h3>${dado.name}</h3>
+                <p>${statusEmoji} ${dado.status} - ${dado.species}</p>
+            </div>`
+                
             galeriaPersonagens.appendChild(card)
         })
 
-        document.getElementById("paginas").textContent = `${dados.info.count} resultados encontrados\nPágina ${dados.info.next - 1 || dados.info.prev + 1} de ${dados.info.pages}`
-        
-        linkRequisicao = ""
-    } catch (erro) {
-        console.error(`Erro ao buscar dados: ${erro}`)
-    }
+        document.getElementById("paginas").textContent = `${dados.info.count} resultados encontrados`
 }
+
+campoBusca.addEventListener("change", () => { criarCardPersonagem() })
+campoStatus.addEventListener("change", () => { criarCardPersonagem() })
+campoEspecie.addEventListener("change", () => { criarCardPersonagem() })
+campoGenero.addEventListener("change", () => { criarCardPersonagem() })
