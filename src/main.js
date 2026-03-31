157b4633fc978a01cc9ec7661f37cc275de6ea76
Heitor Sales <desenvolvheitor@gmail.com>
2026-03-31 15:56:35 -0300

    Funcionalidade do botão de limpar filtros

--- src/main.js
+++ src/main.js
@@ -1,3 +1,5 @@
+import { criarCardPersonagem } from "./components/card.js"
+
 // Atualização automática de data e hora
 const dataHoraHeader = document.getElementById("data-hora-header")
 
@@ -5,3 +7,15 @@ setInterval(() => {
     let hoje = new Date()
     dataHoraHeader.textContent = `${hoje.toLocaleString("pt-br", { dateStyle: "long", timeStyle: "medium" }).replace("às", "•")}` 
 }, 1000)
+
+criarCardPersonagem()
+
+// Funcionamento do botão de Limpar filtros na busca do personagem
+let botaoLimparFiltros = document.getElementById("botao-limpar-filtros")
+botaoLimparFiltros.addEventListener("click", () => {
+    document.getElementById("campo-busca").value = ""
+    document.getElementById("status").value = ""
+    document.getElementById("especies").value = ""
+    document.getElementById("generos").value = ""
+    criarCardPersonagem()
+})
