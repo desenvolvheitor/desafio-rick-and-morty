7bff4f3a608a517d332b1fff5ac672f73feda4b6
Heitor Sales <desenvolvheitor@gmail.com>
2026-04-01 16:04:46 -0300

    Refatoração para separação de responsabilidades

--- src/api/rickAndMorty.js
+++ src/api/rickAndMorty.js
@@ -1,6 +1,24 @@
-export async function consumirApi(linkRequisicao) {
+export async function buscarPersonagens(filtros) {
     try {
-        let resposta = await fetch(encodeURI(linkRequisicao))
+        const parametros = new URLSearchParams(filtros)
+        const url = `https://rickandmortyapi.com/api/character/?${parametros.toString()}`
+        const resposta = await fetch(url)
+
+        if (!resposta.ok) {
+            throw new Error(`Erro na rede: ${resposta.status}`)
+        }
+
+        const dados = await resposta.json()
+        return dados
+    } catch (erro) {
+        console.error(`Erro ao buscar dados: ${erro}`)
+    }
+}
+
+export async function buscarPersonagemPorId(id) {
+    try {
+        const url = `https://rickandmortyapi.com/api/character/${id}`
+        const resposta = await fetch(url)
 
         if (!resposta.ok) {
             throw new Error(`Erro na rede: ${resposta.status}`)
