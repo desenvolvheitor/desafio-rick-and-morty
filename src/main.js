3de46a16169c95cd5fcef7abedc104e04b4f30be
Heitor Sales <desenvolvheitor@gmail.com>
2026-04-01 23:59:55 -0300

    Transferência de todo o funcionamento principal generalizado para o main.js

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
-import { criarCardPersonagem } from "./components/card.js"
+import { buscarPersonagens } from "./api/rickAndMorty.js"
+import { renderizarGaleria } from "./components/card.js"
 
 // Atualização automática de data e hora
 const dataHoraHeader = document.getElementById("data-hora-header")
@@ -8,14 +9,39 @@ setInterval(() => {
     dataHoraHeader.textContent = `${hoje.toLocaleString("pt-br", { dateStyle: "long", timeStyle: "medium" }).replace("às", "•")}` 
 }, 1000)
 
-criarCardPersonagem()
+const filtros = {
+    name: "",
+    status: "",
+    species: "",
+    gender: "",
+};
 
-// Funcionamento do botão de Limpar filtros na busca do personagem
-let botaoLimparFiltros = document.getElementById("botao-limpar-filtros")
-botaoLimparFiltros.addEventListener("click", () => {
-    document.getElementById("campo-busca").value = ""
-    document.getElementById("status").value = ""
-    document.getElementById("especies").value = ""
-    document.getElementById("generos").value = ""
-    criarCardPersonagem()
-})
+const campoBusca = document.getElementById("campo-busca")
+const campoStatus = document.getElementById("status")
+const campoEspecie = document.getElementById("especies")
+const campoGenero = document.getElementById("generos")
+
+campoBusca.addEventListener("change", () => { 
+    filtros.name = campoBusca.value
+    executarBusca()
+ })
+
+campoStatus.addEventListener("change", () => { 
+    filtros.status = campoStatus.value
+    executarBusca()
+ })
+
+campoEspecie.addEventListener("change", () => { 
+    filtros.species = campoEspecie.value
+    executarBusca()
+ })
+
+campoGenero.addEventListener("change", () => { 
+    filtros.gender = campoGenero.value
+    executarBusca()
+ })
+
+async function executarBusca() {
+    const dados = await buscarPersonagens(filtros)
+    renderizarGaleria(dados.results)
+}
