4e49f4317fb1278e59f993ae62460cd17f5d703c
Heitor Sales <desenvolvheitor@gmail.com>
2026-04-01 22:11:23 -0300

    Separação de responsabilidades (apenas criação de cards)

--- src/components/card.js
+++ src/components/card.js
@@ -1,36 +1,10 @@
-import { consumirApi } from "../api/rickAndMorty.js"
 import { abrirModalPersonagem } from "./modal.js"
 
 const galeriaPersonagens = document.getElementById("galeria-personagens")
-const campoBusca = document.getElementById("campo-busca")
-const campoStatus = document.getElementById("status")
-const campoEspecie = document.getElementById("especies")
-const campoGenero = document.getElementById("generos")
 
-export async function criarCardPersonagem(linkRequisicao = `https://rickandmortyapi.com/api/character/?`) {
-        galeriaPersonagens.innerHTML = "Carregando..."
-        
-        if (campoBusca.value.length > 0) {
-            linkRequisicao += `&name=${campoBusca.value}`
-        }
-
-        if (campoEspecie.value !== "") {
-            linkRequisicao += `&species=${campoEspecie.value}`
-        }
-
-        if (campoStatus.value !== "") {
-            linkRequisicao += `&status=${campoStatus.value}`
-        }
-
-        if (campoGenero.value !== "") {
-            linkRequisicao += `&gender=${campoGenero.value}`
-        }
-
-        const dados = await consumirApi(linkRequisicao)
+export function renderizarGaleria(listaPersonagens) {
         galeriaPersonagens.innerHTML = ""
-        dados.results.forEach((dado, indice) => {
-
-
+        listaPersonagens.forEach((dado, indice) => {
             let card = document.createElement("li")
             card.setAttribute("class", "card-personagem")
             card.onclick = () => abrirModalPersonagem(dado.id)
@@ -41,7 +15,7 @@ export async function criarCardPersonagem(linkRequisicao = `https://rickandmorty
             switch (dado.status.toString().toLowerCase()) {
                 case "alive":
                     statusColor = "var(--alive-color)"
-                    statusEmoji = "&#x1FAC0;"
+                    statusEmoji = "&#x2764;&#xFE0F;"
                     break
                 case "dead":
                     statusColor = "var(--dead-color)"
@@ -55,19 +29,13 @@ export async function criarCardPersonagem(linkRequisicao = `https://rickandmorty
 
             card.style.backgroundColor = statusColor
             
-            card.innerHTML = `<img src="${dado.image}" alt="" class="foto-personagem">
+            card.innerHTML = `<img await src="${dado.image}" alt="" class="foto-personagem">
             <div class="informacoes-card-personagem">
                 <h3>${dado.name}</h3>
-                <p>${statusEmoji} ${dado.status} - ${dado.species}</p>
+                <p>${dado.species}</p>
+                <p>${statusEmoji} ${dado.status}</p>
             </div>`
                 
             galeriaPersonagens.appendChild(card)
         })
-
-        document.getElementById("paginas").textContent = `${dados.info.count} resultados encontrados`
 }
-
-campoBusca.addEventListener("change", () => { criarCardPersonagem() })
-campoStatus.addEventListener("change", () => { criarCardPersonagem() })
-campoEspecie.addEventListener("change", () => { criarCardPersonagem() })
-campoGenero.addEventListener("change", () => { criarCardPersonagem() })
