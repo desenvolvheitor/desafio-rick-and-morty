c60e1d00e320ccc4ba0a84d595f0b2daa18bfd99
Heitor Sales <desenvolvheitor@gmail.com>
2026-04-08 16:06:08 -0300

    Correção com as modificações no arquivo de requisição à api e clicar fora do modal fecha

--- src/components/modal.js
+++ src/components/modal.js
@@ -1,21 +1,33 @@
-import { buscarPersonagemPorId } from "../api/rickAndMorty.js"
-
 const modalPersonagem = document.getElementById("modal-personagem")
 
-export async function abrirModalPersonagem(id) {
-    const dados = await buscarPersonagemPorId(id)
-    modalPersonagem.innerHTML = `<img src="${dados.image}">`
+export async function abrirModalPersonagem(personagem) {
+    modalPersonagem.innerHTML = `<img src="${personagem.image}">`
     modalPersonagem.innerHTML += `<div id="informacoes-modal-personagem">
-        <h2>${dados.name}</h2>
-        <p>Status: ${dados.status}</p>
-        <p>Espécie: ${dados.species}</p>
-        <p>Gênero: ${dados.gender}</p>
-        <p>Origem: ${dados.origin.name}</p>
-        <p>Localização: ${dados.location.name}</p>
-        <p>Episódios: ${dados.episode.length}</p>
+        <h2>${personagem.name}</h2>
+        <p>Status: ${personagem.status}</p>
+        <p>Espécie: ${personagem.species}</p>
+        <p>Gênero: ${personagem.gender}</p>
+        <p>Origem: ${personagem.origin.name}</p>
+        <p>Localização: ${personagem.location.name}</p>
+        <p>Episódios: ${personagem.episode.length}</p>
         </div>
         <div id="botao-fechar-modal" aria-text="Fechar">&#x2715;</div>`
     modalPersonagem.showModal()
     const botaoFecharModal = document.getElementById("botao-fechar-modal")
     botaoFecharModal.onclick =  () => { modalPersonagem.close() }
 }
+
+modalPersonagem.addEventListener("click", (event) => {
+    const areaModal = modalPersonagem.getBoundingClientRect()
+
+    const clicouFora = (
+        event.clientX < areaModal.left ||
+        event.clientX > areaModal.right ||
+        event.clientY < areaModal.top ||
+        event.clientY > areaModal.bottom
+    )
+
+    if (clicouFora) {
+        modalPersonagem.close()
+    }
+})
